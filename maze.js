773bb783e7d6fd64a341386f1f5c60e2a14e0018
anthonywilliamsjj <32628614+anthonywilliamsjj@users.noreply.github.com>
2017-10-20 22:15:53 -0500

    Update maze.js

--- maze.js
+++ maze.js
@@ -1,63 +1,52 @@
+//EXERCISE1
 window.onload;
 
-let boundaryOver = document.getElementById("boundary1");
+let boundaryOver=document.getElementById("boundary1");
 
-boundaryOver.addEventListener("mouseover", function warning() 
-	{
-		boundaryOver.classList.add("youlose");
-	}
+boundaryOver.addEventListener("mouseover", function warning(){
+	boundaryOver.classList.add("youlose");
 )
 
-			
-//Excercise 2
-
-for ( var count = 0; count < document.getElementsByClassName("boundary").length - 1; count++){
+//EXERCISE2
+for (var count=0;count<document.getElementsByClassName("boundary").length-1;count++){
 	document.getElementsByClassName("boundary")[count].addEventListener("mouseover", function control(){
-	for (var n = 0; n < document.getElementsByClassName("boundary").length - 1; n++){
-		document.querySelectorAll("div.boundary")[n].classList.add("youlose");
-	}
-	let newElement = document.createElement("h2");
-	newElement.textContent = "You Lose!";
-	document.body.append(newElement);
-	var status = document.createAttribute("id");
-	status.value = "status";
-	newElement.setAttributeNode(status);	})
+		for (var num=0; num < document.getElementsByClassName("boundary").length-1;num++){
+			document.querySelectorAll("div.boundary")[n].classList.add("youlose");
 		}
+		let newElement = document.createElement("h2");
+		newElement.textContent = "You Lose!";
+		document.body.append(newElement);
+		var status = document.createAttribute("id");
+		status.value = "status";
+		newElement.setAttributeNode(status);	
+	}
+}
 
-//Excercise 3
+//EXERCISE3
 
-let endOfMaze = document.getElementById("end");
-endOfMaze.addEventListener("mouseover", function end()
-		{
-			if (document.getElementsByClassName("youlose").length === 0)
-			{
+let endOfMaze=document.getElementById("end");
+endOfMaze.addEventListener("mouseover", function end(){
+	if (document.getElementsByClassName("youlose").length===0){
+		let newElement=document.createElement("h2");
+		newElement.textContent="You Win!";
+		document.body.append(newElement);
+		var status=document.createAttribute("id");
+		status.value="status";
+		newElement.setAttributeNode(status);
+	}
 
-				let newElement = document.createElement("h2");
-				newElement.textContent = "You Win!";
-				document.body.append(newElement);
-				var status = document.createAttribute("id");
-				status.value = "status";
-				newElement.setAttributeNode(status);	
+)
 
-			}
-		}	
+//EXERCISE4
 
-	)
-
-//Excercise 4
-
-let resetGame = document.getElementById("start");
-resetGame.addEventListener("click", function reset()
-
-	{
-		var i = document.getElementsByClassName("boundary").length - 1;
-		while (i >= 0)
-		{
-			document.querySelectorAll(".boundary")[i].classList.remove("youlose");
-	 		i--;
-	    }
-
-	    	// When the user clicks the start icon, this code, it removes the status either you win or you lose
+let resetGame=document.getElementById("start");
+resetGame.addEventListener("click", function reset(){
+	var i = document.getElementsByClassName("boundary").length - 1;
+	while (i >= 0){
+		document.querySelectorAll(".boundary")[i].classList.remove("youlose");
+		i--;
+	}
+	// When the user clicks the start icon, this code, it removes the status either you win or you lose
 	   if (document.getElementsByTagName("div").innerHTML === `<h2 id = "status"> You Win </h2>` || `<h2 id = "status"> You Lose </h2>`)
 	   			{
 	   				var removeSatus = document.getElementById("status");
