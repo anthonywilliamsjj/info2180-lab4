2ac0d89c5bb3bfb05d30bba4c83846bcd545f7f4
anthonywilliamsjj <32628614+anthonywilliamsjj@users.noreply.github.com>
2017-10-20 22:42:22 -0500

    Update maze.js

--- maze.js
+++ maze.js
@@ -25,19 +25,19 @@ for (var count=0;count<document.getElementsByClassName("boundary").length-1; cou
 }
 
 //EXERCISE3
-let endOfMaze = document.getElementById("end");
+let endOfMaze=document.getElementById("end");
 endOfMaze.addEventListener("mouseover", function end(){
-			if (document.getElementsByClassName("youlose").length===0){
-				let newElement = document.createElement("h2");
-				newElement.textContent = "You Win!";
-				document.body.append(newElement);
-				var status = document.createAttribute("id");
-				status.value = "status";
-				newElement.setAttributeNode(status);	
+	if (document.getElementsByClassName("youlose").length===0){
+		let newElement=document.createElement("h2");
+		newElement.textContent="You Win!";
+		document.body.append(newElement);
+		var status=document.createAttribute("id");
+		status.value="status";
+		newElement.setAttributeNode(status);
+	}
 
-			}
-
-})
+}
+)
 
 
 //EXERCISE4
@@ -49,21 +49,23 @@ resetGame.addEventListener("click", function reset(){
 	 		n--;
 	    }
 	   if (document.getElementsByTagName("div").innerHTML === `<h2 id = "status"> You Win </h2>` || `<h2 id = "status"> You Lose </h2>`){
-	   	var removeSatus = document.getElementById("status"); 
+	   	var removeSatus=document.getElementById("status"); 
 	   	removeSatus.parentNode.removeChild(removeSatus); //removes the status either you win or you lose when clicked
 	   }
 
-})
+}
+)
 
-let loseGame = document.getElementById("maze");
+let loseGame=document.getElementById("maze");
 loseGame.addEventListener("mouseleave", function endGame(){
-	for (var n = 0; n < document.getElementsByClassName("boundary").length - 1; n++){
-		document.querySelectorAll("div.boundary")[n].classList.add("youlose");
+	for (var num=0; num<document.getElementsByClassName("boundary").length - 1; num++){
+		document.querySelectorAll("div.boundary")[num].classList.add("youlose");
 	}
-	let newElement = document.createElement("h2");
-	newElement.textContent = "You Lose!";
+	let newElement=document.createElement("h2");
+	newElement.textContent="You Lose!";
 	document.body.append(newElement);
 	var status = document.createAttribute("id");
 	status.value = "status";
 	newElement.setAttributeNode(status);
-})
+}
+)
